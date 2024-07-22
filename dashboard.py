fe84e34851917c1ba1a3188ff40843a4c48deaef
Sidnei Junior <58188259+sidneijunior7@users.noreply.github.com>
2024-07-22 12:05:15 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -7,14 +7,14 @@ from sqlalchemy import create_engine
 
 # Funções para cálculos
 def calculate_metrics(df):
-    df['Monthly Returns'] = df['Capital'].pct_change()
-    cagr = (df['Capital'].iloc[-1] / df['Capital'].iloc[0]) ** (1 / (len(df) / 12)) - 1
-    monthly_returns = df.resample('M', on='Date')['Capital'].last().pct_change()
+    df['Monthly Returns'] = df['EQUITY'].pct_change()
+    cagr = (df['EQUITY'].iloc[-1] / df['EQUITY'].iloc[0]) ** (1 / (len(df) / 12)) - 1
+    monthly_returns = df.resample('M', on='DATE')['EQUITY'].last().pct_change()
     recovery_factor = (monthly_returns.sum() / abs(monthly_returns[monthly_returns < 0].sum()))
     profit_factor = (monthly_returns[monthly_returns > 0].sum() / abs(monthly_returns[monthly_returns < 0].sum()))
     positive_months = (monthly_returns > 0).sum()
     negative_months = (monthly_returns < 0).sum()
-    max_drawdown = abs(df['Drawdown'].max())
+    max_drawdown = abs(df['EQUITY'].min() - df['EQUITY'].max())
     adjusted_return = monthly_returns.mean() * (2 / max_drawdown) if max_drawdown != 0 else np.nan
 
     return {
@@ -28,24 +28,25 @@ def calculate_metrics(df):
 
 def plot_capital_curve(df):
     fig, ax = plt.subplots()
-    ax.plot(df['Date'], df['Capital'])
+    ax.plot(df['DATE'], df['EQUITY'])
     ax.set_title('Capital Curve')
     ax.set_xlabel('Date')
-    ax.set_ylabel('Capital')
+    ax.set_ylabel('Equity')
     st.pyplot(fig)
 
 def plot_annual_capital_curves(df):
-    df['Year'] = df['Date'].dt.year
-    fig = px.line(df, x='Date', y='Capital', color='Year', title='Annual Capital Curves')
+    df['Year'] = df['DATE'].dt.year
+    fig = px.line(df, x='DATE', y='EQUITY', color='Year', title='Annual Capital Curves')
     st.plotly_chart(fig)
 
 # Streamlit UI
 st.title('Daytrade Backtest Dashboard')
 
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+uploaded_file = st.file_uploader("SELECIONE UM ARQUIVO CSV", type="csv")
 
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file, parse_dates=['Date'],  encoding='utf-16')
+    df = pd.read_csv(uploaded_file, encoding="utf-16", parse_dates=['<DATE>'], date_parser=lambda x: pd.to_datetime(x, format='%Y.%m.%d %H.%M'))
+    df.rename(columns={'<DATE>': 'DATA', '<BALANCE>': 'BALANCO', '<EQUITY>': 'CAPITAL LIQUIDO'}, inplace=True)
     st.write("Data preview:", df.head())
 
     # Calculate metrics
